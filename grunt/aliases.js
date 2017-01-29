c16b148d1e1876e38b8fe0af5cb03c416afa067c
Phil Gates-Shannon <phil.gs@gmail.com>
2017-01-29 09:14:18 -0500

    Add robust dynamic alias for executing Metalsmith build scripts

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,8 +1,36 @@
+let _ = require( 'lodash' );
+
 module.exports = function( grunt, options ) {
 
     // --- HELPER FUNCTIONS ---
     let buildHandler = function( target ) {
-        grunt.log.ok().ok( target );
+        let targetList = [
+            'alpha'
+        ];
+
+        if ( !target ) {
+            target = 'all';
+        } else {
+            target = _.camelCase( target );
+        }
+
+        if ( target === 'all' ) {
+            grunt.log.ok( 'Building all targets...' );
+            _.forEach( targetList, executeScript );
+        } else if ( target === 'help' ) {
+            grunt.log.warn( 'Available targets' + _.join( targetList, ', ' ) );
+        } else if ( _.includes( targetList, target ) ) {
+            executeScript( target );
+        } else {
+            grunt.log.error()
+                .error( 'Unknown build target: ' + target )
+                .error( 'Try "build:help" to see a list of valid targets' );
+        }
+    };
+
+    let executeScript = function( name ) {
+        grunt.log.ok( 'Building target ' + name );
+        grunt.task.run( 'run:' + name );
     };
 
     // --- ALIAS DEFINITIONS ---
